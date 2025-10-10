7503d0195bd7b40bb7eef94012d4bf7d5f1e6596
Samir Chowdhary <143581623+SamirChowdhary24@users.noreply.github.com>
2025-10-10 23:30:19 +0530

    Delete azure-pipeline directory

--- azure-pipeline/modules/compute/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "private_ips" {
-  description = "The private IP addresses of the deployed VMs."
-  value       = azurerm_network_interface.nic.*.private_ip_address
-}
--- azure-pipeline/modules/loadbalancer/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "app_gateway_public_ip" {
-  description = "The public IP address of the Application Gateway."
-  value       = azurerm_public_ip.app_gateway_ip.ip_address
-}
